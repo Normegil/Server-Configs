65039a7a30aabc9c76972391dfd2090607f93eb0
Olivier Ory <mail@normegil.be>
2015-06-01 18:52:03 +0200

    Move No-Ip configurations

--- docker/data/Run.sh
+++ docker/data/Run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 docker run \
   -d \
-  -v /home/normegil/Server-Configs/docker/no-ip/config/:/etc/no-ip/ \
-  -v /home/normegil/Server-Configs/docker/no-ip/scripts/:/scripts/no-ip/ \
+  -v /home/normegil/Server-Configs/docker/servers/no-ip/config/:/etc/no-ip/ \
+  -v /home/normegil/Server-Configs/docker/servers/no-ip/scripts/:/scripts/no-ip/ \
   -v /home/normegil/Server-Configs/docker/databases/mysql/config/:/etc/mysql/conf.d/ \
   -v /home/normegil/Server-Configs/docker/databases/mysql/data/:/var/lib/mysql/ \
   --name data \
