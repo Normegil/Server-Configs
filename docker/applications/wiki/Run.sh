fc98296bb31fcb0fda36e3841bcdd21a482b712a
Olivier Ory <mail@normegil.be>
2016-12-14 20:54:17 +0100

    Wiki: Add volumes to Wiki image

--- docker/applications/wiki/Run.sh
+++ docker/applications/wiki/Run.sh
@@ -3,5 +3,7 @@ docker run \
 	-d \
 	-p 3000:80 \
 	-e VIRTUAL_HOST=wiki.normegil.be \
+	-v /home/normegil/Wiki/Pages:/var/dokuwiki-storage/data/pages \
+	-v /home/normegil/Wiki/Configuration:/var/dokuwiki-storage/conf \
 	--name app-wiki \
 	server/wiki
